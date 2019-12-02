c25aece9edf6f561d748e031ca818fb571557494
Maarten <maarten.jansen@ziggo.nl>
2019-12-02 15:15:50 +0100

    Removed old module

--- bin/MG_BQC.py
+++
@@ -1,5 +0,0 @@
-if __name__ == "__main__":
-	programm= "checkm"
-	os.chdir(outdir)
-	command= "checkm lineage_wf -t "+threads+" --pplacer_threads "+threads+" -x .fa -f checkm/extended_summary.txt bin/ checkm/"
-	run(command)
