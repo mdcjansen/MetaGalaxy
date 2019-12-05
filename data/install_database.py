5896c540d4ff738248e472794ab0489dcd5d1fdc
Maarten <maarten.jansen@ziggo.nl>
2019-12-05 09:08:38 +0100

    Fixed database installer

--- data/install_database.py
+++ data/install_database.py
@@ -3,8 +3,8 @@ import os,sys
 
 if __name__ == "__main__":
 	__author__= "M.D.C. Jansen"
-	__version__= "MetaGalaxy v1.0"
-	__date__= "1st of November, 2019"
+	__version__= "MetaGalaxy database downloader v1.1"
+	__date__= "5th of December, 2019"
 	bin_dir = "bin"
 	data_dir= "data"
 	lib_dir = "lib"
@@ -13,16 +13,16 @@ if __name__ == "__main__":
 	bin= os.path.join(root, bin_dir)
 	data= os.path.join(root, data_dir)
 	lib= os.path.join(root, lib_dir)
-	os.system(lib+"/kraken2/kraken2-build --download-taxonomy archaea --db "+data+"/kraken2_pdb --protein --threads 28")
-	os.system(lib+"/kraken2/kraken2-build --download-taxonomy bacteria --db "+data+"/kraken2_pdb --protein --threads 28")
-	os.system(lib+"/kraken2/kraken2-build --download-taxonomy plasmid --db "+data+"/kraken2_pdb --protein --threads 28")
-	os.system(lib+"/kraken2/kraken2-build --download-taxonomy viral --db "+data+"/kraken2_pdb --protein --threads 28")
-	os.system(lib+"/kraken2/kraken2-build --download-taxonomy human --db "+data+"/kraken2_pdb --protein --threads 28")
-	os.system(lib+"/kraken2/kraken2-build --download-taxonomy fungi --db "+data+"/kraken2_pdb --protein --threads 28")
-	os.system(lib+"/kraken2/kraken2-build --download-taxonomy plant --db "+data+"/kraken2_pdb --protein --threads 28")
-	os.system(lib+"/kraken2/kraken2-build --download-taxonomy protozoa --db "+data+"/kraken2_pdb --protein --threads 28")
-	os.system(lib+"/kraken2/kraken2-build --download-taxonomy env_nr --db "+data+"/kraken2_pdb  --protein --threads 28")
-	os.system(lib+"/kraken2/kraken2-build --download-taxonomy env_nt --db "+data+"/kraken2_pdb --protein --threads 28")
+	os.system(lib+"/kraken2/kraken2-build --download-library archaea --db "+data+"/kraken2_pdb --protein --threads 28")
+	os.system(lib+"/kraken2/kraken2-build --download-library bacteria --db "+data+"/kraken2_pdb --protein --threads 28")
+	os.system(lib+"/kraken2/kraken2-build --download-library plasmid --db "+data+"/kraken2_pdb --protein --threads 28")
+	os.system(lib+"/kraken2/kraken2-build --download-library viral --db "+data+"/kraken2_pdb --protein --threads 28")
+	os.system(lib+"/kraken2/kraken2-build --download-library human --db "+data+"/kraken2_pdb --protein --threads 28")
+	os.system(lib+"/kraken2/kraken2-build --download-library fungi --db "+data+"/kraken2_pdb --protein --threads 28")
+	os.system(lib+"/kraken2/kraken2-build --download-library plant --db "+data+"/kraken2_pdb --protein --threads 28")
+	os.system(lib+"/kraken2/kraken2-build --download-library protozoa --db "+data+"/kraken2_pdb --protein --threads 28")
+	os.system(lib+"/kraken2/kraken2-build --download-library env_nr --db "+data+"/kraken2_pdb  --protein --threads 28")
+	os.system(lib+"/kraken2/kraken2-build --download-library env_nt --db "+data+"/kraken2_pdb --protein --threads 28")
 	os.system(lib+"/kraken2/kraken2-build --build --db "+data+"/kraken2_pdb --threads 28")
 	os.system(lib+"/kraken2/kraken2-build --standard --db "+data+"/kraken2_ndb --threads 28")
 	os.chdir(data)
